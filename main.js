b0afe1186a25f329b0b43742ac4f3778fbeb1f17
Brendan Freeman <bfreeman0@gmail.com>
2024-01-07 16:22:28 +0000

    basics

--- main.js
+++ main.js
@@ -2,17 +2,35 @@ const myLibrary = []
 const bookTable = document.querySelector('#bookTable')
 const addBookButton = document.querySelector('#addABook')
 
+const updateList = (array) => {
+  return (
+    array.map(book => {
+      return `<div class="bookRow"><span class="bookName">${book.name}</span><span class="bookAuthor">${book.author}</span><input type="button" value="delete" class="deleteButton"></input></div>`
+    }).join(''))
+}
 
-class Book{
-  constructor(name, author){
+class Book {
+  constructor(name, author) {
     this.name = name
     this.author = author
   }
 
-  addBookToLibrary(){
+  addBookToLibrary() {
     myLibrary.push(this)
   }
+
+
+
 }
+const deleteBook = (book, array)=>{
+  const position = array.indexOf(book)
+  array.splice(position, 1)
+  updateList(array)
+  console.log(array)
+
+
+}
+const bookListHeader = "<div class='bookRow'><span>Title</span><span>Author</span><span>Delete</span></div>"
 
 const theFinalEmpire = new Book("The Final Empire", "Brandon Sanderson")
 const theWheelOfAscension = new Book("The Wheel of Ascension", "Brandon Sanderson")
@@ -20,23 +38,21 @@ const theWheelOfAscension = new Book("The Wheel of Ascension", "Brandon Sanderso
 theFinalEmpire.addBookToLibrary()
 theWheelOfAscension.addBookToLibrary()
 
-const bookList =
-  myLibrary.map(book =>{
-    return `<div class="bookRow"><span class="bookName">${book.name}</span><span class="bookAuthor">${book.author}</span></div>`
-  }).join('')
-
-console.log(myLibrary[0].name)
-
-const updateList = (array)=>{
-  return(
-  array.map(book => {
-    return `<div class="bookRow"><span class="bookName">${book.name}</span><span class="bookAuthor">${book.author}</span></div>`
-  }).join(''))
+const handleDelete = (event)=>{
+  console.log(event)
 }
 
-bookTable.innerHTML = bookList
+const bookList =
+  myLibrary.map(book => {
+    const bookRow = document
+    return `<div class="bookRow"><span class="bookName">${book.name}</span><span class="bookAuthor">${book.author}</span><input type="button" value="Delete" class="deleteButton" id=${myLibrary.indexOf(book)} onClick=${handleDelete(this)}></input></div>`
+  }).join('')
 
-const addABook = ()=>{
+
+
+bookTable.innerHTML = bookListHeader + bookList
+
+const addABook = () => {
   const dialogBox = document.createElement('dialog')
   const dialogBoxHeader = document.createElement('h1')
   const bookNameSection = document.createElement('div')
@@ -60,27 +76,27 @@ const addABook = ()=>{
   bookAuthorSection.append(bookAuthorLabel)
 
   bookSubmit.textContent = 'Submit'
-  bookSubmit.addEventListener('click', ()=>{
+  bookSubmit.addEventListener('click', () => {
     const newBook = new Book(bookNameEntry.value, bookAuthorEntry.value)
     newBook.addBookToLibrary()
     dialogBox.remove()
-    bookTable.innerHTML = updateList(myLibrary)
+    bookTable.innerHTML = bookListHeader + updateList(myLibrary)
     console.log(myLibrary)
   })
 
   dialogBox.appendChild(bookNameSection)
   dialogBox.appendChild(bookAuthorSection)
   dialogBox.appendChild(bookSubmit)
-  
+
   dialogBox.classList.add('bookDialog')
   bookNameSection.classList.add('dialogSection')
   bookAuthorSection.classList.add('dialogSection')
-  
+
   body.appendChild(dialogBox)
 
   dialogBox.showModal()
 
-  
+
 }
 
 addBookButton.addEventListener('click', addABook)
